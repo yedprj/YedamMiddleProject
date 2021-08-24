61948c25d255649eb4db1e37fa1a941782ff4daf
lkh <moonline176@gmail.com>
2021-08-24 09:56:20 +0900

    날짜, 버튼 기능

--- coby/src/main/java/co/pyl/coby/board/command/BoardInsert.java
+++ coby/src/main/java/co/pyl/coby/board/command/BoardInsert.java
@@ -2,7 +2,6 @@ package co.pyl.coby.board.command;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import co.pyl.coby.board.service.BoardService;
 import co.pyl.coby.board.serviceImpl.BoardServiceImpl;
@@ -15,13 +14,13 @@ public class BoardInsert implements Command {
 	@Override
 	//게시글 추가
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		HttpSession session = request.getSession();
 		BoardService dao = new BoardServiceImpl();
 		BoardVO vo = new BoardVO();
 		
 		vo.setUserId(request.getParameter("userId"));;
 		vo.setBoardTitle(request.getParameter("boardTitle"));
 		vo.setBoardContent(request.getParameter("boardContent"));
+		
 		
 		dao.boardInsert(vo);
 		return "boardList.do";
